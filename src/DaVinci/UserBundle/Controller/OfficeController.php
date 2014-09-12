fa5c74def1ba345f8445a5e8aaefd486dc726967
Nickolay <icevita@gmail.com>
2014-09-12 19:35:23 +0300

    All.js code fixed routes hanges everybody should run command composer update and php app/console cache:clear

--- src/DaVinci/UserBundle/Controller/OfficeController.php
+++ src/DaVinci/UserBundle/Controller/OfficeController.php
@@ -3,24 +3,41 @@
 namespace DaVinci\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class OfficeController extends Controller
 {
+    /**
+    * @Route("/choose-office", name="fos_user_registration_confirmed")
+    * @Method("GET")
+    * @Security("has_role('ROLE_USER') or has_role('ROLE_TAXIDRIVER') or has_role('ROLE_COMPANYDRIVER') or has_role('ROLE_TAXIMANAGER') or has_role('ROLE_TAXICOMPANY')")
+    */
     public function office_chooseAction()
     {
         return $this->render('DaVinciUserBundle:Offices:office_choose.html.twig');
     }
     
+    /**
+    * @Security("has_role('ROLE_TAXICOMPANY')")
+    */
     public function office_companyAction()
     {
         return $this->render('DaVinciUserBundle:Offices:office_company.html.twig');
     }
-       
+    
+    /**
+    * @Security("has_role('ROLE_USER')")
+    */
     public function office_passengerAction()
     {
         return $this->render('DaVinciUserBundle:Offices:office_passenger.html.twig');
     }
-        
+ 
+    /**
+    * @Security("has_role('ROLE_TAXIDRIVER')")
+    */
     public function office_driverAction()
     {
         return $this->render('DaVinciUserBundle:Offices:office_driver.html.twig');
