fc821a154d7c2ab2aa30db18e6d8165bc99b4995
Nickolay <icevita@gmail.com>
2014-09-24 01:47:57 +0300

    uikit js updated, mainpage tab switcher added

--- src/DaVinci/TaxiBundle/Resources/public/js/addons/sortable.js
+++ src/DaVinci/TaxiBundle/Resources/public/js/addons/sortable.js
@@ -1,4 +1,4 @@
-/*! UIkit 2.8.0 | http://www.getuikit.com | (c) 2014 YOOtheme | MIT License */
+/*! UIkit 2.10.0 | http://www.getuikit.com | (c) 2014 YOOtheme | MIT License */
 
 /*
   * Based on nativesortable - Copyright (c) Brian Grinstead - https://github.com/bgrins/nativesortable
@@ -27,7 +27,7 @@
         return ('draggable' in div) || ('ondragstart' in div && 'ondrop' in div);
     })(),
 
-    draggingPlaceholder, moving, clickedlink;
+    draggingPlaceholder, moving, dragging, clickedlink, delayIdle;
 
     // disable native dragndrop support for now
     supportsDragAndDrop = false;
@@ -38,6 +38,7 @@
 
             warp             : false,
             animation        : 150,
+            threshold        : 10,
 
             childClass       : 'uk-sortable-item',
             placeholderClass : 'uk-sortable-placeholder',
@@ -45,7 +46,6 @@
             draggingClass    : 'uk-sortable-dragged',
             dragMovingClass  : 'uk-sortable-moving',
             dragCustomClass  : '',
-
             handleClass      : false,
 
             stop             : function() {},
@@ -67,18 +67,25 @@
             } else {
 
                 // prevent leaving page after link clicking
+                // prevent leaving page after link clicking
                 this.element.on('mousedown touchstart', 'a[href]', function(e) {
-                    clickedlink = $(this);
+                    // don't break browser shortcuts for click+open in new tab
+                    if(!e.ctrlKey && !e.metaKey && !e.shiftKey) {
+                        clickedlink = $(this);
+                    }
                 }).on('click', 'a[href]', function(e) {
-                    clickedlink = $(this);
-                    e.stopImmediatePropagation();
-                    return false;
+                    if(!e.ctrlKey && !e.metaKey && !e.shiftKey) {
+                        clickedlink = $(this);
+                        e.stopImmediatePropagation();
+                        return false;
+                    }
                 });
             }
 
             var handleDragStart = delegate(function(e) {
 
                 moving = false;
+                dragging = false;
 
                 var target = $(e.target), children = $this.element.children();
 
@@ -91,7 +98,7 @@
                     var handle = target.hasClass($this.options.handleClass) ? target : target.closest('.'+$this.options.handleClass, element);
 
                     if (!handle.length) {
-                        e.preventDefault();
+                        //e.preventDefault();
                         return;
                     }
                 }
@@ -109,25 +116,35 @@
 
                 var $current = $(currentlyDraggingElement), offset = $current.offset();
 
-                draggingPlaceholder = $('<div class="'+([$this.options.draggingClass, $this.options.dragCustomClass].join(' '))+'"></div>').css({
-                    display : 'none',
-                    top     : offset.top,
-                    left    : offset.left,
-                    width   : $current.width(),
-                    height  : $current.height(),
-                    padding : $current.css('padding')
-                }).data('mouse-offset', {
-                    'left': offset.left - parseInt(e.pageX, 10),
-                    'top' : offset.top  - parseInt(e.pageY, 10)
-                }).append($current.html()).appendTo('body');
+                delayIdle = {
 
-                draggingPlaceholder.$current  = $current;
-                draggingPlaceholder.$sortable = $this;
+                    pos       : { x:e.pageX, y:e.pageY },
+                    threshold : $this.options.threshold,
+                    'apply'   : function() {
 
-                addFakeDragHandlers();
+                        draggingPlaceholder = $('<div class="'+([$this.options.draggingClass, $this.options.dragCustomClass].join(' '))+'"></div>').css({
+                            display : 'none',
+                            top     : offset.top,
+                            left    : offset.left,
+                            width   : $current.width(),
+                            height  : $current.height(),
+                            padding : $current.css('padding')
+                        }).data('mouse-offset', {
+                            'left': offset.left - parseInt(e.pageX, 10),
+                            'top' : offset.top  - parseInt(e.pageY, 10)
+                        }).append($current.html()).appendTo('body');
 
-                $this.options.start(this, currentlyDraggingElement);
-                $this.trigger('sortable-start', [$this, currentlyDraggingElement]);
+                        draggingPlaceholder.$current  = $current;
+                        draggingPlaceholder.$sortable = $this;
+
+                        addFakeDragHandlers();
+
+                        $this.options.start(this, currentlyDraggingElement);
+                        $this.trigger('sortable-start', [$this, currentlyDraggingElement]);
+
+                        delayIdle = false;
+                    }
+                }
 
                 if (!supportsDragAndDrop) {
                     e.preventDefault();
@@ -185,6 +202,7 @@
 
             var handleDrop = delegate(function(e) {
 
+
                 if (e.type === 'drop') {
 
                     if (e.stopPropagation) {
@@ -196,7 +214,7 @@
                     }
                 }
 
-                if (this === currentlyDraggingElement) {
+                if (!dragging) {
                     return;
                 }
 
@@ -339,6 +357,8 @@
 
         moveElementNextTo: function(element, elementToMoveNextTo) {
 
+            dragging = true;
+
             var $this    = this,
                 list     = $(element).parent().css('min-height', ''),
                 next     = isBelow(element, elementToMoveNextTo) ? elementToMoveNextTo : elementToMoveNextTo.nextSibling,
@@ -347,7 +367,7 @@
 
             if($this.options.warp || !$this.options.animation) {
                 elementToMoveNextTo.parentNode.insertBefore(element, next);
-                $(document).trigger("uk-check-display");
+                UI.Utils.checkDisplay($this.element);
                 return;
             }
 
@@ -387,7 +407,7 @@
                             count--
                             if (!count) {
                                 list.css('min-height', '');
-                                $(document).trigger("uk-check-display");
+                                UI.Utils.checkDisplay(ele);
                             }
                         });
                     }, 0);
@@ -447,9 +467,9 @@
     }
 
     // auto init
-    $(document).on("uk-domready", function(e) {
+    UI.ready(function(context) {
 
-        $("[data-uk-sortable]").each(function(){
+        $("[data-uk-sortable]", context).each(function(){
 
           var ele = $(this);
 
@@ -459,7 +479,13 @@
         });
     });
 
-    $(document).on('mousemove touchmove', function(e) {
+    UI.$doc.on('mousemove touchmove', function(e) {
+
+        if (delayIdle) {
+            if (Math.abs(e.pageX - delayIdle.pos.x) > delayIdle.threshold || Math.abs(e.pageY - delayIdle.pos.y) > delayIdle.threshold) {
+                delayIdle.apply();
+            }
+        }
 
         if (draggingPlaceholder) {
 
@@ -481,13 +507,13 @@
         }
     });
 
-    $(document).on('mouseup touchend', function() {
+    UI.$doc.on('mouseup touchend', function() {
 
         if(!moving && clickedlink) {
             location.href = clickedlink.attr('href');
         }
 
-        clickedlink = false;
+        delayIdle = clickedlink = false;
     });
 
     return UI.sortable;
