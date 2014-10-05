c11b6a58b7b9efa60cc3493b647cc84af987d8a5
Evgeny <busforward@gmail.com>
2014-10-06 01:23:50 +0600

    edit select car color

--- src/DaVinci/TaxiBundle/Resources/public/js/all.js
+++ src/DaVinci/TaxiBundle/Resources/public/js/all.js
@@ -1251,3 +1251,19 @@ $('.chois li').change(function () {
 	
     	EYE.register(initLayout, 'init');
 })(jQuery)
+
+ //Выбор цвета авто ////////////////////////
+$(window).bind("load", function() {
+  $(".my-select").chosenImage({
+    disable_search_threshold: 10 
+  });
+});
+jQuery.browser = {};
+(function () {
+    jQuery.browser.msie = false;
+    jQuery.browser.version = 0;
+    if (navigator.userAgent.match(/MSIE ([0-9]+)\./)) {
+        jQuery.browser.msie = true;
+        jQuery.browser.version = RegExp.$1;
+    }
+})();
