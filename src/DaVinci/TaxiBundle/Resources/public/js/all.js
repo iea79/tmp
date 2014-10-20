bfb901a91aac1d89bc5111a29dbc15bdfae81f5b
Alla <alla_88@inbox.lv>
2014-10-20 18:58:28 +0200

    Small adjustments

--- src/DaVinci/TaxiBundle/Resources/public/js/all.js
+++ src/DaVinci/TaxiBundle/Resources/public/js/all.js
@@ -234,7 +234,7 @@ $('.spec-request .uk-parent > a, .auto-tip a').click(function(event){
 //simbols left in textarea in step 2 homepage
 var text_area_len = 200;
 
-$('.wishes-1 textarea, .wishes-2 textarea').keyup(function(){
+$('.wishes-1 textarea, .wishes-2 textarea, .about-passenger textarea').keyup(function(){
     if($(this).val().length > text_area_len){
         $(this).val($(this).val().substr(0, text_area_len));
 		}
