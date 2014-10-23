125192e3720ac62f37e2f7f4a1198544cd834f14
Nickolay <icevita@gmail.com>
2014-10-23 03:19:32 +0300

    js fixed

--- src/DaVinci/TaxiBundle/Resources/public/js/datarange.js
+++ src/DaVinci/TaxiBundle/Resources/public/js/datarange.js
@@ -1,42 +1,4 @@
 // Datapicker
-/**
- *
- * Date picker
- * Author: Stefan Petre www.eyecon.ro
- * 
- * Dual licensed under the MIT and GPL licenses
- * 
- */
- (function($){
-	var EYE = window.EYE = function() {
-		var _registered = {
-			init: []
-		};
-		return {
-			init: function() {
-				$.each(_registered.init, function(nr, fn){
-					fn.call();
-				});
-			},
-			extend: function(prop) {
-				for (var i in prop) {
-					if (prop[i] != undefined) {
-						this[i] = prop[i];
-					}
-				}
-			},
-			register: function(fn, type) {
-				if (!_registered[type]) {
-					_registered[type] = [];
-				}
-				_registered[type].push(fn);
-			}
-		};
-	}();
-	$(EYE.init);
-})(jQuery);
-
-
 //The main Datapicker  
 (function ($) {
 	var DatePicker = function () {
@@ -875,6 +837,44 @@
   };
 })();
 
+
+/**
+ *
+ * Date picker
+ * Author: Stefan Petre www.eyecon.ro
+ * 
+ * Dual licensed under the MIT and GPL licenses
+ * 
+ */
+ (function($){
+	var EYE = window.EYE = function() {
+		var _registered = {
+			init: []
+		};
+		return {
+			init: function() {
+				$.each(_registered.init, function(nr, fn){
+					fn.call();
+				});
+			},
+			extend: function(prop) {
+				for (var i in prop) {
+					if (prop[i] != undefined) {
+						this[i] = prop[i];
+					}
+				}
+			},
+			register: function(fn, type) {
+				if (!_registered[type]) {
+					_registered[type] = [];
+				}
+				_registered[type].push(fn);
+			}
+		};
+	}();
+	$(EYE.init);
+})(jQuery);
+
 (function($){
 	var initLayout = function() {
 		var hash = window.location.hash.replace('#', '');
@@ -939,4 +939,5 @@
 	}; 
 	
     	EYE.register(initLayout, 'init');
+        $(EYE.init);
 })(jQuery);
---
+++ src/DaVinci/TaxiBundle/Resources/public/js/gmaps.js
@@ -0,0 +1,5 @@
+define('gmaps', ['async!googlemaps'],
+function(){
+    // return the gmaps namespace for brevity
+    return window.google.maps;
+});
