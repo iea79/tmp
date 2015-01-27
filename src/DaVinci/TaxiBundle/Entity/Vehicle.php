c96b7e43b0426dd0cdd0a4e885c1aeef19c0f105
eplayorg <eplayorg@bitbucket.org>
2015-01-27 11:33:24 +0100

    Bug from tickets 4286, 4287 have been fixed. 	modified:   src/DaVinci/TaxiBundle/Entity/Vehicle.php 	modified:   src/DaVinci/TaxiBundle/Resources/views/Home/createPassengerRequest.html.twig 	modified:   src/DaVinci/TaxiBundle/Resources/views/Home/order_step_2.html.twig

--- src/DaVinci/TaxiBundle/Entity/Vehicle.php
+++ src/DaVinci/TaxiBundle/Entity/Vehicle.php
@@ -17,22 +17,13 @@ class Vehicle {
 	const POS_CLASS_STANDART = 4;
 	const POS_CLASS_FULL_SIZE = 5;
 	const POS_CLASS_PREMIUM = 6;
-	const POS_CLASS_MIDSIZE_SUV = 7;
-	const POS_CLASS_STANDART_CONVERTIBLE = 8;
-	const POS_CLASS_PREMIUM_PICKUP = 9;
-	const POS_CLASS_MINI_VAN = 10;
-	const POS_CLASS_STANDART_SUV = 11;
-	const POS_CLASS_STANDART_VAN = 12;
-	const POS_CLASS_FULL_SIZE_PICKUP = 13;
-	const POS_CLASS_FULL_SIZE_SUV = 14;
-	const POS_CLASS_STANDART_PICKUP = 15;
-	const POS_CLASS_FULL_SIZE_VAN = 16;
-	const POS_CLASS_SPECIAL_VAN = 17;
-	const POS_CLASS_PREMIUM_VAN = 18;
-	const POS_CLASS_LUXURY = 19;
-	const POS_CLASS_PREMIUM_SPECIAL = 20;
-	const POS_CLASS_PREMIUM_SUV = 21;
-	const POS_CLASS_SPECIAL = 22;
+	const POS_CLASS_LUXURY = 7;
+	const POS_CLASS_OTHER = 8;
+	const POS_CLASS_CONVERTIBLE = 9;
+	const POS_CLASS_VAN = 10;
+	const POS_CLASS_SUV = 11;
+	const POS_CLASS_SPECIALITY = 12;
+	const POS_CLASS_PICKUP = 13;
 	
 	const CLASS_DEFAULT = 'Not chosen';
 	const CLASS_ECONOMY = 'Economy';
@@ -41,23 +32,14 @@ class Vehicle {
 	const CLASS_STANDART = 'Standart';
 	const CLASS_FULL_SIZE = 'Full Size';
 	const CLASS_PREMIUM = 'Premium'; 
-	const CLASS_MIDSIZE_SUV = 'Midsize SUV';
-	const CLASS_STANDART_CONVERTIBLE = 'Standart Convertible';
-	const CLASS_PREMIUM_PICKUP = 'Premium Pickup'; 
-	const CLASS_MINI_VAN = 'Mini Van'; 
-	const CLASS_STANDART_SUV = 'Standart SUV';
-	const CLASS_STANDART_VAN = 'Standart Van';
-	const CLASS_FULL_SIZE_PICKUP = 'Full Size Pickup';
-	const CLASS_FULL_SIZE_SUV = 'Full Size SUV';
-	const CLASS_STANDART_PICKUP = 'Standart Pickup';
-	const CLASS_FULL_SIZE_VAN = 'Full Size Van';
-	const CLASS_SPECIAL_VAN = 'Special Van';
-	const CLASS_PREMIUM_VAN = 'Premium Van';
 	const CLASS_LUXURY = 'Luxury';
-	const CLASS_PREMIUM_SPECIAL = 'Premium Special';
-	const CLASS_PREMIUM_SUV = 'Premium SUV';
-	const CLASS_SPECIAL = 'Special';
-	
+	const CLASS_OTHER = 'Other';
+	const CLASS_CONVERTIBLE = 'Convertible';
+	const CLASS_VAN = 'Van';
+	const CLASS_SUV = 'SUV';
+	const CLASS_SPECIALITY = 'Speciality';
+	const CLASS_PICKUP = 'Pickup';
+		
 	/**
 	 * @ORM\Id
 	 * @ORM\Column(type="integer")
@@ -66,7 +48,7 @@ class Vehicle {
 	private $id;
 	
 	/**
-	 * @ORM\Column(type="string", columnDefinition="ENUM('Not chosen', 'Economy', 'Compact', 'Midsize', 'Standart', 'Full Size', 'Premium', 'Midsize SUV', 'Standart Convertible', 'Premium Pickup', 'Mini Van', 'Standart SUV', 'Standart Van', 'Full Size Pickup', 'Full Size SUV', 'Full Size Van', 'Special Van', 'Premium Van', 'Luxury', 'Premium Special', 'Premium SUV', 'Special')", name="vehicle_class", length=100)
+	 * @ORM\Column(type="string", columnDefinition="ENUM('Not chosen', 'Economy', 'Compact', 'Midsize', 'Standart', 'Full Size', 'Premium', 'Luxury', 'Other', 'Convertible', 'Speciality', 'Pickup')", name="vehicle_class", length=100)
 	 */
 	private $vehicleClass;
 	
@@ -163,7 +145,7 @@ class Vehicle {
     {
         return $this->passengerRequest;
     }
-    
+        
     public static function getChoices()
     {
     	return array(
@@ -174,22 +156,13 @@ class Vehicle {
     		self::POS_CLASS_STANDART => self::CLASS_STANDART,
     		self::POS_CLASS_FULL_SIZE => self::CLASS_FULL_SIZE,
     		self::POS_CLASS_PREMIUM => self::CLASS_PREMIUM,
-    		self::POS_CLASS_MIDSIZE_SUV => self::CLASS_MIDSIZE_SUV,
-    		self::POS_CLASS_STANDART_CONVERTIBLE => self::CLASS_STANDART_CONVERTIBLE,
-    		self::POS_CLASS_PREMIUM_PICKUP => self::CLASS_PREMIUM_PICKUP,
-    		self::POS_CLASS_MINI_VAN => self::CLASS_MINI_VAN,
-    		self::POS_CLASS_STANDART_SUV => self::CLASS_STANDART_SUV,
-    		self::POS_CLASS_STANDART_VAN => self::CLASS_STANDART_VAN,
-    		self::POS_CLASS_FULL_SIZE_PICKUP => self::CLASS_FULL_SIZE_PICKUP,
-    		self::POS_CLASS_FULL_SIZE_SUV => self::CLASS_FULL_SIZE_SUV,
-    		self::POS_CLASS_STANDART_PICKUP => self::CLASS_STANDART_PICKUP,
-    		self::POS_CLASS_FULL_SIZE_VAN => self::CLASS_FULL_SIZE_VAN,
-    		self::POS_CLASS_SPECIAL_VAN => self::CLASS_SPECIAL_VAN,
-    		self::POS_CLASS_PREMIUM_VAN => self::CLASS_PREMIUM_VAN,
     		self::POS_CLASS_LUXURY => self::CLASS_LUXURY,
-    		self::POS_CLASS_PREMIUM_SPECIAL => self::CLASS_PREMIUM_SPECIAL,
-    		self::POS_CLASS_PREMIUM_SUV => self::CLASS_PREMIUM_SUV,
-    		self::POS_CLASS_SPECIAL => self::CLASS_SPECIAL
+    		self::POS_CLASS_OTHER => self::CLASS_OTHER,
+    		self::POS_CLASS_CONVERTIBLE => self::CLASS_CONVERTIBLE,
+    		self::POS_CLASS_VAN => self::CLASS_VAN,
+    		self::POS_CLASS_SUV => self::CLASS_SUV,
+    		self::POS_CLASS_SPECIALITY => self::CLASS_SPECIALITY,
+    		self::POS_CLASS_PICKUP => self::CLASS_PICKUP
     	);
     }
     
