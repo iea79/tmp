b1415996022e14f9d181a356402c7fdac4eec63e
eplayorg <eplayorg@bitbucket.org>
2015-02-27 01:17:45 +0100

    Google Map integration.

--- src/DaVinci/TaxiBundle/Form/Payment/MakePayment.php
+++ src/DaVinci/TaxiBundle/Form/Payment/MakePayment.php
@@ -5,9 +5,27 @@ namespace DaVinci\TaxiBundle\Form\Payment;
 class MakePayment 
 {
 	
+	private $balanceType;
+	
+	/**
+	 * @var \DaVinci\TaxiBundle\Form\Payment\PaymentMethod
+	 */
 	private $paymentMethod;
 	
+	/**
+	 * @var \DaVinci\TaxiBundle\Form\Payment\Money
+	 */
+	private $money;
 	
+	public function setMoney(\DaVinci\TaxiBundle\Form\Payment\Money $money)
+	{
+		$this->money = $money;
+	}
+	
+	public function getMoney()
+	{
+		return $this->money;
+	}
 	
 }
 
