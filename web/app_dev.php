6c011b547bb3087a0024d32af2c16d231c9d9ab8
eplayorg <eplayorg@bitbucket.org>
2015-06-02 20:32:09 +0200

    Ip addresses list modified.

--- web/app_dev.php
+++ web/app_dev.php
@@ -12,23 +12,17 @@ use Symfony\Component\Debug\Debug;
 if ((isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1'
-        , '80.64.90.73'//home Nickolay
-        , '77.91.130.82'//work Nickolay
-        , '10.240.35.8' //cloud9
         , '81.198.90.13' //dima
         , '37.151.110.103' //elena kontent
         , '95.71.81.69' //elena 
-        , '93.100.166.25' //elena verst
         , '62.16.43.150' //evgeny ivanov
         , '89.209.11.134' //vasya
         , '185.43.248.214' //aleksey
-        , '84.83.255.2' //Alla
         , '109.68.173.20' //evgeniy voronov
         , '95.111.150.84' //Дарья
-        , '83.69.244.205' //Олеся PR
         , '109.87.88.65' //Алексей Гловлюк 
-        , '37.133.94.37' //Evgeny April (Barcelona)
-        , '93.117.143.157' //kontent manager
+        , '37.133.94.73' //Evgeny April (Barcelona)
+        , '93.117.143.157' //Content manager
         , '2.135.194.139' //Elena Ilyna
         , '89.252.8.95'//Yuriy (QA)		
        , 'fe80::1', '::1')) || php_sapi_name() === 'cli-server') ) && !getenv("C9_PROJECT")
