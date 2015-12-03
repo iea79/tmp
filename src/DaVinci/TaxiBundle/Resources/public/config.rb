446bd70e5eb6573ba78aef33316bbce723ba0860
Evgeny Ivanov <busforward@gmail.com>
2015-12-04 03:14:53 +0500

    Add new file sysitem sass

---
+++ src/DaVinci/TaxiBundle/Resources/public/config.rb
@@ -0,0 +1,11 @@
+http_path = ".."
+fonts_dir = "fonts" # by Compass.app
+javascript_dir = "js" 
+css_dir = "css" # by Compass.app 
+sass_dir = "sass" # by Compass.app 
+images_dir = "images" # by Compass.app 
+output_style = :compressed # by Compass.app 
+relative_assets = false # by Compass.app 
+line_comments = false # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = false # by Compass.app 
