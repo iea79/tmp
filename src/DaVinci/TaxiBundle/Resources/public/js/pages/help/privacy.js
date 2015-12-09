3c55efdf875cd4d43bbe3c3fc654e8a5e68a33eb
Evgeny Ivanov <busforward@gmail.com>
2015-12-10 01:43:27 +0500

    Merge 10.12.15

---
+++ src/DaVinci/TaxiBundle/Resources/public/js/pages/help/privacy.js
@@ -0,0 +1,18 @@
+require(["pages/common"], function ($) {
+   
+            $('#filter').keyup(function () {
+
+                    var rex = new RegExp($(this).val(), 'i');
+                    $('.privacy .search_res').hide();
+                    $('.privacy .search_res').filter(function () {
+                        return rex.test($(this).find('h4').text()) || rex.test($(this).find('a').text()) || rex.test($(this).find('.text').text()) || rex.test($(this).find('.search_res').text());
+                        
+                    }).show();
+
+            })
+            $('#filter').keyup();
+            
+   togglePreloader(document.body, false);
+});
+
+
