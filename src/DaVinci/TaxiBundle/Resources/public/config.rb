144bb44b9fc2669da3522f135ee7072f631f81f8
Evgeny Ivanov <busforward@gmail.com>
2015-12-12 00:39:32 +0500

    Удалял баги. Не компилировался css

--- src/DaVinci/TaxiBundle/Resources/public/config.rb
+++ src/DaVinci/TaxiBundle/Resources/public/config.rb
@@ -1,6 +1,4 @@
 http_path = ".."
-fonts_dir = "fonts" # by Compass.app
-javascript_dir = "js" 
 css_dir = "css" # by Compass.app 
 sass_dir = "sass" # by Compass.app 
 images_dir = "images" # by Compass.app 
